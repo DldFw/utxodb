e182e4b446ee1eb2768367c93dfe7f2ffe34eb2f
foursealee <913357989@qq.com>
2020-10-07 15:23:38 +0800

    init project

---
+++ include/syncer.h
@@ -0,0 +1,62 @@
+#ifndef SYNCER_H_
+#define SYNCER_H_
+
+#include "job/job.h"
+#include "job/task.h"
+#include "rpc.h"
+
+class Syncer:public Task
+{
+public:
+    Syncer()
+    {
+		init_mempool_ = false;
+    }
+
+    virtual ~Syncer()
+    {
+
+    }
+
+    static Syncer& instance()
+    {
+        return single_;
+    }
+
+	void appendBlockSql(const json& json_block, const uint64_t& height, std::vector<std::string>& vect_txid);
+
+	void appendTxVinVoutSql(const json& json_tx, const std::string& txid);
+	void appendBlockToDB(const json& json_block, const uint64_t& height);
+	void refreshDB();
+
+	void scanBlockChain();
+    
+    void verifyTransaction();
+
+	void scanMempool();
+
+	void setBeginEnd(const uint64_t& begin, const uint64_t& end)
+	{
+		begin_ = begin;
+		end_ = end;
+	}
+
+public:
+
+    void registerTask(map_event_t& name_events, map_job_t& name_tasks);
+
+    void setRpc(const Rpc& rpc)
+	{
+		rpc_ = rpc; 
+	}
+
+protected:
+    static Syncer single_;
+	Rpc rpc_;
+	std::vector<std::string> vect_sql_;
+	std::map<std::string, bool> map_mempool_tx_;
+	bool init_mempool_ = false;
+	uint64_t begin_ ,end_;
+};
+
+#endif
---
+++ src/syncer.cpp
@@ -0,0 +1,131 @@
+#include "syncer.h"
+#include <glog/logging.h>
+#include "db_mysql.h"
+#include <gmp.h>
+#include <gmpxx.h>
+std::map<std::string, int> s_map_address_id;
+static void SetTimeout(const std::string& name, int second)
+{
+    struct timeval timeout ;
+    timeout.tv_sec = second;
+    timeout.tv_usec = 0;
+    evtimer_add(Job::map_name_event_[name], &timeout);
+}
+
+static void ScanChain(int fd, short kind, void *ctx)
+{
+    LOG(INFO) << "scan block begin ";
+
+    Syncer::instance().scanBlockChain(); 
+    SetTimeout("ScanChain", 10*60);
+}
+
+
+void Syncer::refreshDB()
+{
+    LOG(INFO) << "refresh DB begin" ;
+    LOG(INFO) << "SQL size: " << vect_sql_.size() ;
+    if (vect_sql_.size() > 0)
+    {
+        g_db_mysql->batchRefreshDB(vect_sql_);
+        vect_sql_.clear();
+    }   
+    LOG(INFO) << "refresh DB end" ;
+}
+
+void Syncer::scanBlockChain()
+{
+    //check height which is needed to upate
+	
+	uint64_t pre_height = 0;
+	std::string sql = "select height from block order by height desc limit 1;";
+	std::map<int,DBMysql::DataType> map_col_type;
+	map_col_type[0] = DBMysql::INT;
+
+	json json_data;
+	g_db_mysql->getData(sql, map_col_type, json_data);
+	if (json_data.size() > 0)
+	{
+		pre_height = json_data[0][0].get<uint64_t>();
+	}
+
+
+	uint64_t cur_height = 0;
+	rpc_.getBlockCount(cur_height);
+    json json_block;
+
+    for (int i = pre_height + 1; i <= cur_height; i++)
+    {
+        json_block.clear();
+
+        rpc_.getBlock(i, json_block);
+        if (g_node_dump)
+        {
+            g_node_dump = false;
+            begin_ = pre_height;
+            break;  
+        }
+		json json_txs = json_block["result"]["tx"];
+		for (int j = 0; j < json_txs.size(); j++)
+		{
+			std::string txid = json_txs[j].get<std::string>();
+			json json_tx;
+			rpc_.getRawTransaction(txid, json_tx);
+
+			json json_vins = json_tx["result"]["vin"];
+			json json_vouts = json_tx["result"]["vout"];
+
+			for (int k = 0; k < json_vins.size(); k++)
+			{
+				if (json_vins[k].find("txid") == json_vins.end())
+				{
+					continue;
+				}
+				std::string del_txid = json_vins[k]["txid"].get<std::string>();
+				int del_n = json_vins[k]["n"].get<int>();
+
+				std::string sql = "DELETE FROM unspent WHERE txid ='" +del_txid+ "' AND '"+ std::to_string(del_n) +"';";
+				vect_sql_.push_back(sql);
+			}
+			
+			for (int k = 0; k < json_vouts.size(); k++)
+			{
+				double value = json_vouts[k]["value"].get<double>();
+
+				if (value  <= 0.000000001 )
+				{
+					continue;
+				}
+				
+				int n = json_vouts[k]["n"].get<int>();
+				std::string pubkey = json_vouts[k]["scriptPubKey"]["hex"].get<std::string>();
+				std::string address = json_vouts[k]["scriptPubKey"]["addresses"][0].get<std::string>();
+
+				std::string sql  = "INSERT INTO `unspent` VALUES ('" + txid  + "', '"+std::to_string(n)+"', '"+ std::to_string(value) +"', '"+ address +"', '" + pubkey +"');";
+				vect_sql_.push_back(sql);
+			}
+		}
+        //LOG(INFO) << "block height: " << i;
+        if(i % 100 == 0 || i == cur_height)
+        {
+
+            LOG(INFO) << "block height: " << i;
+            std::string timestamps = json_block["result"]["timestamp"].get<std::string>();
+            std::string hash = json_block["result"]["hash"].get<std::string>();
+            //INSERT INTO `xsvdb`.`block` (`height`, `timestamps`) VALUES ('23', '123123');
+            std::string sql = "INSERT INTO `block` VALUES ('" + std::to_string(i) + "');";
+            vect_sql_.push_back(sql);
+            refreshDB();
+        }
+    }
+
+}
+
+
+Syncer Syncer::single_;
+void Syncer::registerTask(map_event_t& name_events, map_job_t& name_tasks)
+{
+    REFLEX_TASK(ScanChain);
+}
+
+
